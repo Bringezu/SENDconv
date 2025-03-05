d2c27e5c2e277562ac743c9b98b22764c1a4e406
Frank Bringezu <M148068@ONE.merckgroup.com>
2025-03-05 13:56:26 +0100

    batch.R updated to allow list of indirs to be processed

--- batch.R
+++ batch.R
@@ -7,17 +7,27 @@ load_all()
 
 # define directories
 # indir <- data source directory
-# indir<-'/opt/provantis/DA/LIVE/'
-indir<-'c:/temp/MQUEST_SEND/Provantis/DA/LIVE/'
+
+## list of indirs is provided below
+indir<-c('/opt/provantis/DA/LIVE/', '/opt/provantis/DA/ARCH/','/opt/provantis/IV/LIVE/', '/opt/provantis/IV/ARCH/')
+
+
+
+# indir<-'c:/temp/MQUEST_SEND/Provantis/DA/LIVE/'
+
+for (i in seq_along(indir)) {
+  
 
 # outdir target directory for storing the SEND files
-outdir<-paste0(indir, 'output/')
+outdir<-paste0(indir[i], 'output/')
 
 # load configuration from local project environment
 c<-config()
 
 for (i in seq_along(c$run)) {
-  d<-loadDomain(c$name[i], indir)
+  d<-loadDomain(c$name[i], indir[i])
   dd<-get(paste0(c$run[i]))(d)
   exportDomain(dd, 'txt', outdir)
+}
+
 }
