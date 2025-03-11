62354fd556ea105c198f2393cc2bd50d31947a2e
Frank Bringezu <M148068@ONE.merckgroup.com>
2025-03-11 10:04:17 +0100

    filter files per date (only files not older than 7 days)

--- R/loadDomain.R
+++ R/loadDomain.R
@@ -1,7 +1,7 @@
 #' loadDomain
 #'
 #' @param domain A character vector that contains the acronym for the domain
-#' @param dir The directory name containng the input files
+#' @param dir The directory name containing the input files
 #'
 #' @return data frame containing the data from the input file
 #' @examples
@@ -21,7 +21,15 @@ loadDomain <- function(domain, dir=dir, pattern=pattern, dec=dec){
   
   # pattern<-paste0('^SEND_',domain,"-.*.csv")
   # pattern<-paste0('^',domain,"_BASF_.*.CSV")
-  files <- list.files(dir, pattern=pattern,ignore.case = T, full.names=TRUE)
+  # files <- list.files(dir, pattern=pattern,ignore.case = T, full.names=TRUE)
+  
+  # filter files per date (only files not older than 7 days)
+  max_days<-7
+  files <- list.files(dir,pattern=pattern,ignore.case = T, full.names=TRUE) %>% 
+    file.info() %>%
+    rownames_to_column() %>% 
+    filter(as.Date(ctime) > Sys.Date()-max_day)
+  
   df <- lapply(files, data.table::fread, dec=dec)
   
   return(df)
