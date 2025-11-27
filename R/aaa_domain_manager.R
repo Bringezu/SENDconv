d67e718b7383f7dddd51edf3ad127d6080fe4a9f
Frank Bringezu <M148068@ONE.merckgroup.com>
2025-11-27 10:23:26 +0100

    Added EG Domain for conversion

--- R/aaa_domain_manager.R
+++ R/aaa_domain_manager.R
@@ -1,10 +1,10 @@
 #'Create a new environment and define a tibble with Acronyms and functions
 the<-new.env(parent = emptyenv())
-the$conf<-tibble::tibble(name=c('BW', 'CL','CV', 'DM','DS', 'EX','FC','LB', 'MA', 'MI', 'OM', 'RE','TA','TS',  'VS'), 
-                            run=c('convertBW', 'convertCL','convertCV', 'convertDM','convertDS', 'convertEX','convertFW','convertLB', 'convertMA', 'convertMI', 'convertOM', 'convertRE','convertTA','convertTS', 'convertVS'))
-
-the$conf<-tibble::tibble(name=c('BW', 'CL','DM','EX','FM','FX', 'MA', 'MI', 'OM', 'PY'), 
-                         run=c('convert_BASF_BW', 'convert_BASF_CL', 'convert_BASF_DM', 'convert_BASF_EX','convert_BASF_FM','convert_BASF_FX', 'convert_BASF_MA', 'convert_BASF_MI', 'convert_BASF_OM', 'convert_BASF_PY'))
+the$conf<-tibble::tibble(name=c('BW', 'CL','CV', 'DM','DS','EG', 'EX','FC','LB', 'MA', 'MI', 'OM', 'RE','TA','TS',  'VS'), 
+                            run=c('convertBW', 'convertCL','convertCV', 'convertDM','convertDS', 'convertEG', 'convertEX','convertFW','convertLB', 'convertMA', 'convertMI', 'convertOM', 'convertRE','convertTA','convertTS', 'convertVS'))
+# 
+# the$conf<-tibble::tibble(name=c('BW', 'CL','DM','EX','FM','FX', 'MA', 'MI', 'OM', 'PY'), 
+#                          run=c('convert_BASF_BW', 'convert_BASF_CL', 'convert_BASF_DM', 'convert_BASF_EX','convert_BASF_FM','convert_BASF_FX', 'convert_BASF_MA', 'convert_BASF_MI', 'convert_BASF_OM', 'convert_BASF_PY'))
 
 
 
