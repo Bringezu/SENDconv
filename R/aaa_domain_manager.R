6867d3d87182f1bb6ac01f0f05abc6b9726d737a
Frank Bringezu <M148068@ONE.merckgroup.com>
2025-11-27 10:32:31 +0100

    EG Domain included in conversion calls

--- R/aaa_domain_manager.R
+++ R/aaa_domain_manager.R
@@ -1,7 +1,9 @@
 #'Create a new environment and define a tibble with Acronyms and functions
 the<-new.env(parent = emptyenv())
-the$conf<-tibble::tibble(name=c('BW', 'CL','CV', 'DM','DS', 'EX','FC','LB', 'MA', 'MI', 'OM', 'RE','TA','TS',  'VS'), 
-                            run=c('convertBW', 'convertCL','convertCV', 'convertDM','convertDS', 'convertEX','convertFW','convertLB', 'convertMA', 'convertMI', 'convertOM', 'convertRE','convertTA','convertTS', 'convertVS'))
+the$conf<-tibble::tibble(name=c('BW', 'CL','CV', 'DM','DS','EG', 'EX','FC','LB', 'MA', 'MI', 'OM', 'RE','TA','TS',  'VS'), 
+                         run=c('convertBW', 'convertCL','convertCV', 'convertDM','convertDS', 'convertEG', 'convertEX','convertFW','convertLB', 'convertMA', 'convertMI', 'convertOM', 'convertRE','convertTA','convertTS', 'convertVS'))
+
+
 
 #'Report configuration
 #'@export
